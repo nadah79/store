8d97e32d98c7cb69162338f291fa609c5859cf3a
nadah79 <nada.hesham.sh@gmail.com>
2022-10-26 02:56:25 +0200

    update

--- e_commerce/lib/main.dart
+++ e_commerce/lib/main.dart
@@ -1,9 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:e_commerce/home_screen.dart';
-// import 'package:e_commerce/list_screen.dart';
-// import 'package:e_commerce/main_screen.dart';
-// import 'package:e_commerce/products.dart';
 import 'package:flutter/material.dart';
 
 void main() {
